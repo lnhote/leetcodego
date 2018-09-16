c2448c46ed9ad747d2a48dd12e62ddc480bda060
林浩特 <lnhote@gmail.com>
2018-09-17 00:47:54 +0800

    wild card

--- actest/test.go
+++ actest/test.go
@@ -14,13 +14,21 @@ var (
 )
 
 func (t SimpleTest) Errorf(format string, args ...interface{}) {
-    fmt.Printf(format, args)
+    fmt.Printf(format, args...)
 }
 
 func Equal(expected, actual interface{}, msgAndArgs ...interface{}) bool {
-    return assert.Equal(t, expected, actual, msgAndArgs)
+    return assert.Equal(t, expected, actual, msgAndArgs...)
+}
+
+func True(actual bool, msgAndArgs ...interface{}) bool {
+    return assert.True(t, actual, msgAndArgs...)
+}
+
+func False(actual bool, msgAndArgs ...interface{}) bool {
+    return assert.False(t, actual, msgAndArgs...)
 }
 
 func EqualValues(expected, actual interface{}, msgAndArgs ...interface{}) bool {
-    return assert.EqualValues(t, expected, actual, msgAndArgs)
+    return assert.EqualValues(t, expected, actual, msgAndArgs...)
 }
